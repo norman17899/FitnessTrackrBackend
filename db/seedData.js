7100e9d07416aba4fa68748e908b3abe341ce646
Shan <norman_17899@yahoo.com>
2023-02-02 08:57:22 -0600

    updated table

--- db/seedData.js
+++ db/seedData.js
@@ -44,6 +44,7 @@ async function createTables() {
     CREATE TABLE routines(
       id SERIAL PRIMARY KEY,
       "creatorId"	INTEGER REFERENCES users(id),
+      "isPublic" BOOLEAN DEFAULT false,
       name VARCHAR(255)	UNIQUE NOT NULL,
       goal TEXT NOT NULL
       
